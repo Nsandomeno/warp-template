602bea3ce923c55a288fc683f89ed83aa10541be
Nicholas Sandomeno <nicholassandomeno@Nicholass-MBP.fios-router.home>
2023-01-12 23:19:30 -0500

    path param

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use warp::Filter;
 
+async fn hello(param: String) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(format!("Hello {}", param))
+}
+
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let hello = warp::get()
+        .and(warp::path("hello"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and_then(hello);
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
